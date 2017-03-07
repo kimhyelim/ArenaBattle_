0d3432459c6036eea81efbdb2c40a0f66207e46d
kimhyelim <limma0608@naver.com>
2017-03-07 15:51:47 +0900

    Add files via upload

---
+++ Source/WebServeice/Classes/webConnect.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "UObject/NoExportTypes.h"
+#include "webConnect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WEBSERVEICE_API UwebConnect : public UObject
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
